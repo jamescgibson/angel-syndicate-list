88a9a406a191bc1d8e86801aaba1e47044b14cbe
James Gibson <james@spacedolphin.io>
2015-12-07 21:03:03 -0500

    Fixes #4 - Cache Pages in Memory

--- bin/export.rb
+++ bin/export.rb
@@ -1,7 +1,11 @@
 #!/usr/bin/env ruby
 require "rubygems"
 require "bundler/setup"
-require_relative "../syndicate_list" 
+require_relative "../syndicate_list"
+require "logger"
 Bundler.require
+
+LOGGER = Logger.new(STDOUT)
+LOGGER.level = defined?(LOG_LEVEL) ? LOG_LEVEL : Logger::INFO
 
 SyndicateList.new.export
